799fb67e194799fdebbe21fc7586d6321848eb6f
PranavPachpute15 <pranavdpachpute15@gmail.com>
2026-02-05 19:37:46 +0530

    Added Majority Element - Boyer Moore voting algorithm

---
+++ Arrays/MajorityElement.java
@@ -0,0 +1,26 @@
+// Problem: Majority Element
+// Platform: LeetCode
+// Topic: Arrays
+// Difficulty: Easy
+// Approach: Boyer-Moore Voting Algorithm
+// Time: O(n) | Space: O(1)
+class Solution {
+    public int majorityElement(int[] nums) {
+        int count = 0;
+        int candidate = 0;
+
+        for (int num : nums) {
+            if (count == 0) {
+                candidate = num;
+            }
+
+            if (num == candidate) {
+                count++;
+            } else {
+                count--;
+            }
+        }
+
+        return candidate;
+    }
+}
